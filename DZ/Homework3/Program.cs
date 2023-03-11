3be870eecec93634e62e17a3d2e140a38c34c8df
Timur <t.mollaev75@yandex.ru>
2023-03-11 19:21:29 +0300

    Homework3 done

--- DZ/Homework3/Program.cs
+++ DZ/Homework3/Program.cs
@@ -1,18 +1,56 @@
-﻿void Polyan(int P)
-{
-int nam1 = P/10000;
-int nam2 = P/1000;
-nam2 = nam2%10;
-int nam4 = P/10; 
-nam4 = nam4%10;
-int nam5 = P%10;
-if (nam1=nam4) 
- if(nam2=nam5)
-  Console.WriteLine($"{P} -> Да");
-else
-   Console.WriteLine($"{P} -> Нет"); 
-}   
-Console.WriteLine("Input five-digit number");
-int P = Convert.ToInt32(Console.ReadLine());
-Polyan(P);
+﻿// Напишите программу, которая принимает на вход пятизначное
+// число и проверяет, является ли оно палиндромом.
 
+// void Polyan(int P)
+// {
+// int nam1 = P/10000;
+// int nam2 = P/1000;
+// nam2 = nam2%10;
+// int nam4 = P/10; 
+// nam4 = nam4%10;
+// int nam5 = P%10;
+//    if ((nam1 == nam5) && (nam2 == nam4))
+
+//          Console.WriteLine($"{P} -> Да");
+// else
+//    Console.WriteLine($"{P} -> Нет"); 
+// }   
+// Console.WriteLine("Input five-digit number");
+// int P = Convert.ToInt32(Console.ReadLine());
+// Polyan(P);
+
+// Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+// void Cube( double N)
+// {
+// for (double i =1; i <= N; i++)
+//    Console.WriteLine($"{i} -> {i}^3 -> {Math.Pow(i,3)}");
+// }
+// Console.WriteLine("Input N");
+// double N = Convert.ToDouble(Console.ReadLine());
+// Cube( N);
+
+
+
+// Напишите программу, которая принимает на вход координаты
+// двух точек и находит расстояние между ними в 3D пространстве.
+
+// double Distanse (double x1, double y1, double z1, double x2, double y2, double z2)
+// {
+//    return Math.Round(Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2)),3);
+// }
+// Console.WriteLine("Input coordinate X of A point");
+// double x1 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Input coordinate Y of A point");
+// double y1 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Input coordinate Z of A point");
+// double z1 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Input coordinate X of B point");
+// double x2 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Input coordinate Y of B point");
+// double y2 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Input coordinate Z of B point");
+// double z2 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine($" Distase A, B -> { Distanse(x1, y1, z1, x2, y2, z2) }");
+// Distanse(x1, y1, z1, x2, y2, z2);
