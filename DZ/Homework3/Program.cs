7bf5f0bb1fc0fb5a89dd8bfa68217a167924870d
Timur <t.mollaev75@yandex.ru>
2023-03-11 15:21:55 +0300

    Решаю первую задачу

---
+++ DZ/Homework3/Program.cs
@@ -0,0 +1,18 @@
+﻿void Polyan(int P)
+{
+int nam1 = P/10000;
+int nam2 = P/1000;
+nam2 = nam2%10;
+int nam4 = P/10; 
+nam4 = nam4%10;
+int nam5 = P%10;
+if (nam1=nam4) 
+ if(nam2=nam5)
+  Console.WriteLine($"{P} -> Да");
+else
+   Console.WriteLine($"{P} -> Нет"); 
+}   
+Console.WriteLine("Input five-digit number");
+int P = Convert.ToInt32(Console.ReadLine());
+Polyan(P);
+
