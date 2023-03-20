765b9d285c7dc04369de31687cff754f68e0e2e6
Timur <t.mollaev75@yandex.ru>
2023-03-20 09:03:35 +0300

    Homework6 is not done

---
+++ DZ/Homework6/Program.cs
@@ -0,0 +1,25 @@
+﻿
+int [] RandomArray (int Size, int Min, int Max)
+{
+    int Size = M;
+    int [] newArr = new int [Size];
+    for (int i = 0; i < Size; i++)
+        Console.WriteLine("Input digits");
+        newArr[i] = Convert.ToInt32(Console.ReadLine());
+}        
+    return newArr;
+    
+void Even (int [] arr)
+{
+ int nam = 0;
+ for (int i = 0; i < arr.Length; i++)
+  {  
+     if (arr[i] % 2 > 0) 
+         nam++;
+  }      
+Console.WriteLine($"Number of even numbers: {nam}");
+}
+Console.WriteLine("Input M");
+int M  = Convert.ToInt32(Console.ReadLine());
+int[arr] = RandomArray( Size, Min, Max);
+void Even (arr); 
