6875c9808785ea00efde65231cb671a99c2a0c04
Timur <t.mollaev75@yandex.ru>
2023-03-21 19:23:33 +0300

    Homework6 not completed

--- DZ/Homework6/Program.cs
+++ DZ/Homework6/Program.cs
@@ -1,25 +1,30 @@
 ﻿
-int [] RandomArray (int Size)
-{
-    int [] newArr = new int [Size];
-    for (int i = 0; i < Size; i++)
-        Console.WriteLine("Input digits");
-        newArr[i] = Convert.ToInt32(Console.ReadLine());
-     return newArr;   
-}        
+// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// int [] Array (int Size)
+// {
+//     int [] newArr = new int [Size];
+//   
+//     for ( int i = 0; i < Size; i++)
+//         newArr[i] = Convert.ToInt32(Console.ReadLine());
+//      return newArr;   
+// }        
     
     
-void Even (int [] Arr)
-{
- int nam = 0;
- for (int i = 0; i < Arr.Length; i++)
-  {  
-     if (Arr[i] > 0) 
-         nam++;
-  }      
-Console.WriteLine($"Number of even numbers: {nam}");
-}
-Console.WriteLine("Input M");
-int Size  = Convert.ToInt32(Console.ReadLine());
-int[] Arr = RandomArray( Size);
-Even(Arr); 
+// void GreaterZero (int [] Arr)
+// {
+//  int nam = 0;
+//  for (int i = 0; i < Arr.Length; i++)
+//   {  
+//      if (Arr[i] > 0) 
+//          nam++;
+//   }      
+// Console.WriteLine($"{nam} Numbers greater then 0");
+// }
+// Console.WriteLine("Input M");
+// int Size  = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input M digits");
+// int[] Arr = Array( Size);
+// GreaterZero(Arr); 
+
+ 
